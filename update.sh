91e2bb06d594e18b57169029f402d6455e49ca9b
十一月 <114818117+syybi@users.noreply.github.com>
2023-06-03 20:20:40 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 echo "Halo2.0一键更新脚本"
-echo "脚本作者：舒夏"
-echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
+echo "脚本作者：飔梦"
+echo "GitHub开源地址：https://github.com/syybi/halo"
 rm -rf halo.jar && wget -O halo.jar https://down.l04.repl.co/dl/halo-2.6.0-rc.1.jar
 echo "更新至Halo2.6.0-rc.1版本，更新完毕！"
 echo "点击Run按钮重新启动本项目！！！"
