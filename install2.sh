752587393adb917a62b6a57a7b30cf21030a2ebd
十一月 <114818117+syybi@users.noreply.github.com>
2023-06-03 20:51:45 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,10 +4,10 @@ echo "GitHub开源地址：https://github.com/syybi/halo"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/syybi/halo/raw/master/install2/.replit
-wget -O replit.nix https://github.com/syybi/halo/raw/master/install2/replit.nix
+wget -O .replit https://raw.githubusercontent.com/syybi/halo/main/.replit
+wget -O replit.nix https://raw.githubusercontent.com/syybi/halo/master/install2/replit.nix
 cd ..
-wget -O main.sh https://github.com/syybi/halo/raw/master/install2/main.sh
+wget -O main.sh https://raw.githubusercontent.com/syybi/halo/master/install2/main.sh
 wget -O halo.jar https://down.l04.repl.co/dl/halo-2.6.0-rc.1.jar
 cp -r build/.replit . && cp -r build/replit.nix .
 rm -rf build/
